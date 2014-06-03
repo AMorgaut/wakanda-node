35a4849ca99435225bd17ca21418443d34e480b6
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-06-03 17:49:14 +0200

    update binding('contextify') for 'vm' API support

---
+++ binding/contextify/contextWorker.js
@@ -0,0 +1,34 @@
+﻿// Initialize a node friendly context
+require('wakanda-node');
+
+self.onmessage = function () {
+    // arguments is used to not pollute the scope with more names
+    // TODO add arguments[0].data.filename in stack traces
+
+    if (arguments[0].data.sandboxArray) {
+        // initialize the context with the sanbox
+        arguments[0].data.sandboxArray.forEach(function (prop) {
+            Object.defineProperty(global, prop.name, {
+                get: function () {
+                    return prop.value;
+                },
+                set: function (newValue) {
+                    prop.value = newValue;
+                }
+            });    
+        });
+    }
+
+    if (arguments[0].data.code) {
+        self.postMessage({
+            // run the code and return the result
+            result: eval(arguments[0].data.code),
+            // also return the potentially modified sandbox
+            sandbox: arguments[0].data.sandboxArray
+        });
+    }
+
+    if (arguments[0].data.close) {
+        self.close();
+    }
+};
--- binding/contextify/index.js
+++ binding/contextify/index.js
@@ -1,1 +1,150 @@
-﻿/** * @module binding(contexify) * * @experimental * * Used by the module "vm" * First goal is to be able to run node.js unit tests to check node API implementation in wakanda-node * Doesn't behave exactly like the node.js version (but doesn't behave the same between different versions of node by the way...) * Doesn't provide a real sandbox but one that is expected to be enhanced in the future (ES6 Proxy would really help) * * @see http://nodejs.org/api/vm.html **/// The binding provides a few useful primitives:// - ContextifyScript(code, { filename = "evalmachine.anonymous",//                            displayErrors = true } = {})//   with methods://   - runInThisContext({ displayErrors = true } = {})//   - runInContext(sandbox, { displayErrors = true, timeout = undefined } = {})// - makeContext(sandbox)// - isContext(sandbox)// From this we build the entire documented API.exports.ContextifyScript = ContextifyScript;exports.makeContext = makeContext;exports.isContext = isContext;/** * @warning NOT IMPLEMENTED * * @param {Object} sandbox * @return Object **/function makeContext(sandbox) {}/** * @warning NOT IMPLEMENTED * * @param (Object) sandbox * @return boolean **/function isContext(sandbox) {    return sandbox instanceof ContextifyScript;}/** * @warning NOT IMPLEMENTED * * @class ContextifyScript * * @constructor * @param {string} code * @param {Object} options **/function ContextifyScript(code, options) {    options = options || {};    options.filename = options.filename || "evalmachine.anonymous";    if (!options.hasOwnProperty('displayErrors')) {        options.displayErrors = true;    }        this.code = code;}/** * @warning NOT IMPLEMENTED * * @method runInThisContext * @param {Object} options **/ContextifyScript.prototype.runInThisContext = eval;/** * @warning NOT IMPLEMENTED * * @method runInContext * @param {Object} sandbox * @param {Object} options **/ContextifyScript.prototype.runInContext = function runInContext(sandbox, options) {    return eval('with (sandbox) {\n' + this.code + '\n}');};
+﻿/**
+ * @module binding(contextify)
+ *
+ * @experimental
+ *
+ * Used by the module "vm"
+ * First goal is to be able to run node.js unit tests to check node API implementation in wakanda-node
+ * May not behave exactly like the node.js version (but doesn't behave the same between different versions of node by the way...)
+ * 
+ * Use Workers to create real dedicated global contexts per proposed sandbox objects
+ *
+ * @see http://nodejs.org/api/vm.html
+ **/
+
+// The binding provides a few useful primitives:
+// - ContextifyScript(code, { filename = "evalmachine.anonymous",
+//                            displayErrors = true } = {})
+//   with methods:
+//   - runInThisContext({ displayErrors = true } = {})
+//   - runInContext(sandbox, { displayErrors = true, timeout = undefined } = {})
+// - makeContext(sandbox)
+// - isContext(sandbox)
+// From this we build the entire documented API.
+
+if (typeof __dirname === 'undefined') {
+    __dirname = File(module.filename).parent.path;
+}
+
+var sandboxes = [];
+var contexts = [];
+
+exports.ContextifyScript = ContextifyScript;
+exports.makeContext = makeContext;
+exports.isContext = isContext;
+exports.deleteContext = deleteContext;
+
+/**
+ * @class Context
+ * @constructor
+ * @param {Object} sandbox
+ */
+function Context(sandbox) {
+    // transform sandbox to an array
+    var sandboxArray = Object.getOwnPropertyNames(sandbox).map(function (name) {
+        return {name: name, value: sandbox[name]};
+    });
+
+    this.worker = new Worker(__dirname + 'contextWorker.js');
+
+    // send the sandbox and code to the prepared context/worker
+    this.worker.postMessage({sandboxArray: sandboxArray});
+    
+    sandboxes.push(sandbox);
+    contexts.push(this);
+}
+
+/**
+ * @param {Object} sandbox
+ **/
+function makeContext(sandbox) {
+    new Context(sandbox);
+}
+
+/**
+ * @param (Object) sandbox
+ * @return boolean
+ **/
+function isContext(sandbox) {
+    return sandboxes.indexOf(sandbox) > -1;
+}
+
+/**
+ * @param {Object} sandbox
+ **/
+function deleteContext(sandbox) {
+    var index = sandboxes.indexOf(sandbox);
+    if (index === -1) return false;
+    contexts[index].worker.postMessage({close: true});
+    delete sandboxes[index];
+    delete contexts[index];
+    return true;
+}
+
+/**
+ * The code is not compiled as in node.js but the Context is really sandboxed and initialized once
+ *
+ * @class ContextifyScript
+ *
+ * @constructor
+ * @param {string} code
+ * @param {Object} options
+ **/
+function ContextifyScript(code, options) {
+
+    this.filename = options.filename || "evalmachine.anonymous";
+    this.displayErrors = options && options.displayErrors;
+    if (typeof this.displayErrors !== 'boolean') {
+        this.displayErrors = true;
+    }
+    this.code = code;
+    this.sandbox = {};
+
+}
+
+
+/**
+ * @method runInThisContext
+ * @param {Object} options
+ **/
+ContextifyScript.prototype.runInThisContext = function runInThisContext(options) {
+    return this.runInContext(this.sandbox, options);
+};
+
+
+/**
+ * @method runInContext
+ * @param {Object} sandbox
+ * @param {Object} options
+ **/
+ContextifyScript.prototype.runInContext = function runInContext(sandbox, options) {
+    // TODO manage this.displayError / options.displayError
+    
+    var context, result;
+    
+    if (!isContext(sandbox)) {
+        makeContext(sandbox);
+    }
+    context = contexts[sandboxes.indexOf(sandbox)];
+
+    // set the worker callback to receive the result and send it back
+    context.worker.onmessage = function (event) {
+        result = event.data.result;
+        event.data.sandboxArray.forEach(function (prop) {
+            // update the sandbox
+            sandbox[prop.name] = prop.value;
+        });
+        exitWait();
+    };
+
+    // send the sandbox and code to the prepared context/worker
+    context.worker.postMessage({
+        filename: options.filename || this.filename,
+        code: this.code
+    });
+    // wait for the result from the worker (runInContext is synchronous)
+    wait();
+    return result;
+};
+
+
