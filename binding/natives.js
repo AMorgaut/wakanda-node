477435bb1082c1bfa02e46c55d231a184899674a
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-06-16 12:16:02 +0200

    add binding('natives')
    load internal modules source code

---
+++ binding/natives.js
@@ -0,0 +1,38 @@
+﻿
+var 
+    __filename, 
+    __dirname, 
+    ROOT, 
+    LIB, 
+    LIB_NODE;
+
+__filename = module.uri;
+__dirname = File(__filename).parent.path;
+
+ROOT = Folder(__dirname).parent.path;
+LIB = Folder(ROOT + 'lib/');
+LIB_NODE = Folder(ROOT + 'node/lib/');
+
+/**
+ * @param {File} file
+ **/
+function addSourceGetter(file) {
+    Object.defineProperty(exports, file.nameNoExt, {
+        get: function () {
+            return loadText(file.path);
+        },
+        enumerable: true
+    });
+}
+
+/**
+ * @param {File} file
+ **/
+function checkSourceGetter(file) {
+    if (!exports.hasOwnProperty(file.nameNoExt)) {
+        addSourceGetter(file);
+    }
+}
+
+LIB.forEachFile(addSourceGetter);
+LIB_NODE.forEachFile(checkSourceGetter);
