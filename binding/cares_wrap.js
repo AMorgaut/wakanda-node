d90663fa3502256605c6cc723ff0d8e3a00b99cf
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-07-11 13:44:31 +0200

    add cares_wrap with IP regex

---
+++ binding/cares_wrap.js
@@ -0,0 +1,14 @@
+﻿
+function isIpv4(address) {
+  return /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(item);
+}
+
+function isIpv6(address) {
+  return /(?:(?:[a-f\d]{1,4}:)*(?:[a-f\d]{1,4}|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(?:(?:[a-f\d]{1,4}:)*[a-f\d]{1,4})?::(?:(?:[a-f\d]{1,4}:)*(?:[a-f\d]{1,4}|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))?)/.test(item);
+}
+
+exports.isIP = function isIP(address) {
+    var result;
+    result = isIpv4(address) ? 4 : (isIpv6(address) ? 6 : 0);
+    return result;
+};
